314794ecf00b7cca804235adcb756363a270a0d3
max _sid <maxonleb5@gmail.com>
2024-11-08 21:59:03 +0300

    Create user model

---
+++ models/User.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = 'user'  # Имя таблицы в базе данных
+
+    id = Column(Integer, primary_key=True)  # Столбец id, первичный ключ
+    username = Column(String, nullable=False)     # Столбец username, не может быть null
+    password = Column(String,  nullable=False) # password not null
+    login = Column(String, nullable=False)
+    
