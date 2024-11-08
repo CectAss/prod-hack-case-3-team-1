9d636d16c912919ad715fc2e44d63ac58b9f245f
max _sid <maxonleb5@gmail.com>
2024-11-08 20:41:54 +0300

    Add event table migration

--- config.py
+++ config.py
@@ -1,1 +1,6 @@
-Z_ZOV_Z = "ZZZ"
+import dotenv
+
+dotenv.load_dotenv('.env')
+env_values = dotenv.dotenv_values('.env')
+
+SQLALCHEMY_URL = 'postgresql://' + env_values['DB_USERNAME'] + ':' + env_values['DB_PASSWORD'] + '@' + env_values['DB_HOST'] + '/' + env_values['DB_DBNAME']
