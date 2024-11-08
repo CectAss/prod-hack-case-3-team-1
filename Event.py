a49dcb97f6b02f3169c7d16e5aa8941a278dc98a
CectAss <78093532+CectAss@users.noreply.github.com>
2024-11-08 21:48:47 +0300

    make Event and Ticket classes simmilar 2 part

--- Event.py
+++ Event.py
@@ -3,18 +3,20 @@ from random import choice
 
 class Event:
     
-    def __init__(self, spending=None, date=None, reason=None): # Базовый класс: Event()==None
+    def __init__(self, id=None, spending=None, date=None, reason=None): # Базовый класс: Event()==None
+        self.id = id
         self.spending = spending
         self.date = date
         self.reason = reason
 
-    def fill(self): # Заполняет рандомными значениями из файла конфига переменную типа Event: Event().fill()==Заполненный Event
-        sp = choice(SPENDINGS)
-        d = choice(DATES)
-        r = choice(REASONS)
-        return Event(sp, d, r)
+    def fill(self, id): # Заполняет рандомными значениями из файла конфига переменную типа Event: Event().fill()==Заполненный Event
+        return Event(id,
+                     choice(EVENT_SPENDINGS), 
+                     choice(EVENT_DATES), 
+                     r = choice(EVENT_REASONS))
     
     def all(self): # Функция, возвращающая словарь из всех значений Event'a: Event().fill().all()~~{'spendings': 'asdad', 'date': 'qew', 'reason': 'qew'}
-        return {"spendings":self.spending,
+        return {"if": self.id,
+                "spendings":self.spending,
                 "date":self.date,
                 "reason":self.reason}
--- config.py
+++ config.py
@@ -1,3 +1,10 @@
-SPENDINGS = ["asdad", "asd", "qe"]
-DATES = ["qew", "qwwww", "ee"]
-REASONS = ["ddd", "qew", "hhh"]
+EVENT_SPENDINGS = ["asdad", "asd", "qe"]
+EVENT_DATES = ["qew", "qwwww", "ee"]
+EVENT_REASONS = ["ddd", "qew", "hhh"]
+    
+TICKET_SPENDINGS = []
+TICKET_DATES_ARIVE = []
+TICKET_DATES_COMEBACK = []
+TICKET_RACES_ID = []
+TICKET_HOMES = []
+TICKET_AWAYS = []
