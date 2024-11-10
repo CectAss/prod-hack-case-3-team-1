9e1c8eb075dc11534f1ae5d238fe57f5643cb397
CectAss <78093532+CectAss@users.noreply.github.com>
2024-11-10 04:15:00 +0300

    upd

--- models/Ticket.py
+++ models/Ticket.py
@@ -1,4 +1,4 @@
-from sqlalchemy import TIME, Column, Integer, String
+from sqlalchemy import TIME, Column, Integer, String, TIME
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
 
